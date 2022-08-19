f52e35b5a2143c20df2d0c523776c3d5e13adcc1
Felipe2304 <felipeaxandre936@gmail.com>
2022-08-18 22:57:59 -0300

    implementando api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [listProfile , setProfile] = useState({});
+
+  const api = 'https://api.github.com/users/Felipe2304'
+
+  const myProfile = ()=>{
+    const resultApi = fetch(api)
+    resultApi
+    .then(response => response.json())
+    .then(response => getResult(response) )
+    .catch(error => getResult(error))
+  }
+
+  const getResult = (response)=>{
+    console.log(response)
+  }
+
+   myProfile()
+  
   return (
-    <div className="App">
+    <div className="container-App">
      
     </div>
   );
